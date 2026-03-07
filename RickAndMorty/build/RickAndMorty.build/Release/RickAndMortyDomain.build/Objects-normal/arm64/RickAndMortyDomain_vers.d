d0c2b1af3980b3bbdafabba0bb030ab1e0a63cbc
Andrés Sánchez <and_sg@outlook.com>
2026-03-06 22:56:57 -0500

    Ignore build/ directory

--- RickAndMorty/build/RickAndMorty.build/Release/RickAndMortyDomain.build/Objects-normal/arm64/RickAndMortyDomain_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/asanchez/Developer/Challenges/KISS/ChallengeRickMorty/RickAndMorty/build/RickAndMorty.build/Release/RickAndMortyDomain.build/DerivedSources/RickAndMortyDomain_vers.c
