d4df0a899aaba25a7ce6726f33010f5e72bc1f0f
Andrés Sánchez <and_sg@outlook.com>
2026-03-07 19:18:32 -0500

    Make URLSessionHTTPClient public

--- RickAndMorty/RickAndMortyDomain/URLSessionHTTPClient.swift
+++ RickAndMorty/RickAndMortyDomain/URLSessionHTTPClient.swift
@@ -1,13 +1,13 @@
 import Foundation
 
-final class URLSessionHTTPClient: HTTPClient {
+public final class URLSessionHTTPClient: HTTPClient {
     private let session: URLSession
 
-    init(session: URLSession) {
+    public init(session: URLSession) {
         self.session = session
     }
 
-    func get(from url: URL, completion: @escaping (Result<(Data, HTTPURLResponse), Error>) -> Void) {
+    public func get(from url: URL, completion: @escaping (Result<(Data, HTTPURLResponse), Error>) -> Void) {
         session.dataTask(with: url) { data, response, error in
             if let error {
                 completion(.failure(error))
